a20f80d59bab8ab2b607d1c1081444bbf22060a8
GarethLockett <56639768+GarethLockett@users.noreply.github.com>
2020-03-04 15:40:12 +0800

    UnityEvents
    Added UnityEvents.
    Added usage description and updated sample test script.
    Removed OLD non-MonoBehaviour static version.

--- Assets/SAEArcadeCabinet/Scripts/SAEArcadeCabinetInputTestMB.cs
+++ Assets/SAEArcadeCabinet/Scripts/SAEArcadeCabinetInputTestMB.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-using System;
-using System.Reflection;
-using System.Linq;
+﻿using UnityEngine;
 
 /*
     Script: SAEArcadeCabinetInputTestMB
     Author: Gareth Lockett
     Version: 1.0
-    Description:    Simple script for testing SAEArcadeCabinetInputMB.
+    Description:    Simple script for testing SAE ArcadeMachine inputs.
+                    Includes examples of polling and System Action events.
 */
 
 public class SAEArcadeCabinetInputTestMB : MonoBehaviour
@@ -18,41 +13,41 @@ public class SAEArcadeCabinetInputTestMB : MonoBehaviour
     // Methods
     private void Start()
     {
-        // Listen for events.
-        SAEArcadeCabinetInputMB.playerPressedButton += this.PlayerPressedButton;
-        SAEArcadeCabinetInputMB.playerHeldButton += this.PlayerHeldButton;
-        SAEArcadeCabinetInputMB.playerReleasedButton += this.PlayerReleasedButton;
-        SAEArcadeCabinetInputMB.playerJoystickAxisChanged += this.PlayerJoystickAxisChanged;
-        SAEArcadeCabinetInputMB.configurationFinished += this.ConfigurationFinsihed;
+        // Subscribe event handler methods.
+        SAE.ArcadeMachine.playerPressedButton += this.PlayerPressedButton;
+        SAE.ArcadeMachine.playerHeldButton += this.PlayerHeldButton;
+        SAE.ArcadeMachine.playerReleasedButton += this.PlayerReleasedButton;
+        SAE.ArcadeMachine.playerJoystickAxisChanged += this.PlayerJoystickAxisChanged;
+        SAE.ArcadeMachine.configurationFinished += this.ConfigurationFinsihed;
 
         // If you are only going to use polling, then you need to configure players first! (Probably good to do this for events too)
-        SAEArcadeCabinetInputMB.instance.ConfiguringPlayers();
+        SAE.ArcadeMachine.input.ConfigurePlayers();
     }
 
     private void Update()
     {
         // Example of polling buttons (Eg Checking here if the YELLOW players' button 1 is pressed)
-        if( SAEArcadeCabinetInputMB.instance.PlayerPressingButton( SAEArcadeCabinetInputMB.PlayerColorId.YELLOW_PLAYER, 1 ) == true )
+        if( SAE.ArcadeMachine.input.PlayerPressingButton( SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 1 ) == true )
             { Debug.Log( "Polling: YELLOW player is pressing button 1" ); }
 
         // Example of polling axis (Eg Checking YELLOW players' joystick axis)
-        Debug.Log( "YELLOW joystick axis: " + SAEArcadeCabinetInputMB.instance.PlayerJoystickAxis( SAEArcadeCabinetInputMB.PlayerColorId.YELLOW_PLAYER ) );
+        Debug.Log( "YELLOW joystick axis: " + SAE.ArcadeMachine.input.PlayerJoystickAxis( SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER ) );
     }
     
     // Event handlers.
-    private void PlayerPressedButton( SAEArcadeCabinetInputMB.PlayerColorId playerId, int buttonId )
+    private void PlayerPressedButton( SAE.ArcadeMachine.PlayerColorId playerId, int buttonId )
     {
         Debug.Log( "PlayerPressedButton: " + playerId.ToString() + "  " + buttonId );
     }
-    private void PlayerHeldButton( SAEArcadeCabinetInputMB.PlayerColorId playerId, int buttonId )
+    private void PlayerHeldButton( SAE.ArcadeMachine.PlayerColorId playerId, int buttonId )
     {
         Debug.Log( "PlayerHeldButton: " + playerId.ToString() + "  " + buttonId );
     }
-    private void PlayerReleasedButton( SAEArcadeCabinetInputMB.PlayerColorId playerId, int buttonId )
+    private void PlayerReleasedButton( SAE.ArcadeMachine.PlayerColorId playerId, int buttonId )
     {
         Debug.Log( "PlayerReleasedButton: " + playerId.ToString() + "  " + buttonId );
     }
-    private void PlayerJoystickAxisChanged( SAEArcadeCabinetInputMB.PlayerColorId playerId, Vector2 axis )
+    private void PlayerJoystickAxisChanged( SAE.ArcadeMachine.PlayerColorId playerId, Vector2 axis )
     {
         Debug.Log( "PlayerJoystickAxisChanged: " + playerId.ToString() + "  " + axis );
     }
